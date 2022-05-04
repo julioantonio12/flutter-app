46baaea3e058a9a72b6bb0288d786d7cdacc92f2
Julio González Martín <jagonzalez@getinsoft.com>
2022-05-04 13:13:18 -0500

    Add first title and description

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+// void main() {
+//   runApp(MyApp());
+// }
+
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Text("Primera aplicación"));
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Primera aplicación"),
+        ),
+        body: Text("Esto es mi primer texto en el body"),
+      ),
+    );
   }
 }
