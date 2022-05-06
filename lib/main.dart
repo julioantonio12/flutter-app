5f5971cb0f601f86a93da8a47a431bddc4878a94
Julio González Martín <jagonzalez@getinsoft.com>
2022-05-06 11:14:30 -0500

    Make use of Stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -6,20 +6,36 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  var questionIndex = 0;
+
   void answerQuestion() {
-    print("Respuesta seleccionada.");
+    setState(() {
+      questionIndex += 1;
+    });
+    print(questionIndex);
   }
 
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      "¿Cuál es tu color favorito?",
+      "¿Cuál es tu animal preferido?"
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text("Primera aplicación"),
         ),
         body: Column(children: [
-          Text("¿Esto es una pregunta?"),
+          Text(questions[questionIndex]),
           // RaisedButton()
           ElevatedButton(
             child: Text("Respuesta 1"),
