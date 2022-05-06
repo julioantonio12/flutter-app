94f0833388ce400f0189d5f99f2e84049001d084
Julio González Martín <jagonzalez@getinsoft.com>
2022-05-06 09:14:32 -0500

    Log when user press a button

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,10 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  void answerQuestion() {
+    print("Respuesta seleccionada.");
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -14,7 +18,24 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text("Primera aplicación"),
         ),
-        body: Text("Esto es mi primer texto en el body"),
+        body: Column(children: [
+          Text("¿Esto es una pregunta?"),
+          // RaisedButton()
+          ElevatedButton(
+            child: Text("Respuesta 1"),
+            onPressed: answerQuestion,
+          ),
+          ElevatedButton(
+            child: Text("Respuesta 2"),
+            onPressed: () => print("Respuesta 2 escogida"),
+          ),
+          ElevatedButton(
+            child: Text("Respuesta 3"),
+            onPressed: () {
+              print("Respuesta 3 escogida.");
+            },
+          ),
+        ]),
       ),
     );
   }
