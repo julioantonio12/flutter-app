23b250774cdcd147958b1fcb54e8ba0fb59239f5
Julio González Martín <jagonzalez@getinsoft.com>
2022-05-11 10:38:26 -0500

    Create quiz and result widgets to separate logic

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import './question.dart';
-import 'answer.dart';
+import './quiz.dart';
+import './result.dart';
 
 // void main() {
 //   runApp(MyApp());
@@ -18,41 +18,46 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   var _questionIndex = 0;
 
+  final _questions = const [
+    {
+      'questionText': '¿Cuál es tu color favorito?',
+      'answers': ['Rojo', 'Verde', 'Amarillo', 'Naranja', 'Azúl']
+    },
+    {
+      'questionText': '¿Cuál es tu animal preferido?',
+      'answers': ['Tortuga', 'León', 'Tigre', 'Tiburón']
+    },
+    {
+      'questionText': '¿Quién es el mejor jugador de LoL?',
+      'answers': ['Dzukill', 'Nemesis', 'Faker']
+    },
+  ];
+
   void _answerQuestion() {
     setState(() {
       _questionIndex += 1;
     });
+    if (_questionIndex < _questions.length) {
+      print('Hay más preguntas');
+    }
+
     print(_questionIndex);
   }
 
   @override
   Widget build(BuildContext context) {
-    const questions = [
-      {
-        'questionText': '¿Cuál es tu color favorito?',
-        'answers': ['Rojo', 'Verde', 'Amarillo', 'Naranja', 'Azúl']
-      },
-      {
-        'questionText': '¿Cuál es tu animal preferido?',
-        'answers': ['Tortuga', 'León', 'Tigre', 'Tiburón']
-      },
-      {
-        'questionText': '¿Quién es el mejor jugador de LoL?',
-        'answers': ['Dzukill', 'Nemesis', 'Faker']
-      },
-    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text("Primera aplicación"),
         ),
-        body: Column(children: [
-          Question(questions[_questionIndex]['questionText']), // as String
-          ...(questions[_questionIndex]['answers'] as List<String>)
-              .map((answer) {
-            return Answer(_answerQuestion, answer);
-          }).toList(),
-        ]),
+        body: _questionIndex < _questions.length
+            ? Quiz(
+                answerQuestion: _answerQuestion,
+                questionIndex: _questionIndex,
+                questions: _questions,
+              )
+            : Result(),
       ),
     );
   }
